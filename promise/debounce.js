4b2a24a10986728be4f0eaee2d706a13f87281a9
lipojack <cp000123@gmail.com>
2025-07-04 09:59:30 +0800

    Debounce: bind this scope

--- promise/debounce.js
+++ promise/debounce.js
@@ -2,10 +2,11 @@
 export const debounce = (fn, delay) => {
   let timer;
   return (...args) => {
+    const context = this; // force "this" scope to the return function, in case the setTimeout apply a class function
     clearTimeout(timer);
     timer = setTimeout(() => {
       // Incase the fn need to access itself's attributes, apply give it the power of using this
-      fn.apply(this, args);
+      fn.apply(context, args);
       // For straightforward function, but the fn does not have ability to access it's attributes
       // fn(...args);
     }, delay)
