fc57b92da53e9179915572f1b3c20084e4f7dbe6
lipojack <cp000123@gmail.com>
2025-07-04 17:20:48 +0800

    useDebounce: Added cancel(), fixed wording

--- promise/useDebounce.js
+++ promise/useDebounce.js
@@ -4,26 +4,33 @@ export const useDebounce = (fn, delay) => {
   const timer = useRef(null);
   // memorize and update fn cause fn might change if parent re-render
   const latestFn = useRef(fn);
-  
+
   // make sure the fn is up to date
   useEffect(() => {
     latestFn.current = fn;
   }, [fn]);
 
   const debounced = useCallback((...args) => {
-    if (timer.current) clearTimeout(timer.current);
+    clearTimeout(timer.current);
     timer.current = setTimeout(() => {
       latestFn.current(...args);
     }, delay);
-  // fn is updated by useRef, so the callback is dependet to "delay"
+    // fn is updated by useRef, so the callback is dependent to "delay"
   }, [delay]);
+
+  const cancel = useCallback(() => {
+    // clearTimeout only tells browser to stop the timer with such id
+    clearTimeout(timer.current);
+    // Explictly showing the timer vanish
+    timer.current = null;
+  }, []);
 
   // clean up timer when unmounted
   useEffect(() => {
     return () => {
-      if (timer.current) clearTimeout(timer.current);
+      clearTimeout(timer.current);
     };
   }, []);
 
-  return debounced;
+  return { debounced, cancel };
 };
