16f10cbdb12b7973b472766e68670c7c8a89303d
lipojack <cp000123@gmail.com>
2025-07-04 10:59:44 +0800

    Throttle: Implemented throttle

---
+++ promise/throttle.js
@@ -0,0 +1,12 @@
+export const throttle = (fn, delay) => {
+  let lastCall = 0;
+  return (...args) => {
+    const context = this;
+    const now = Date.now();
+    // Initial call will run instantly cause now - 0 always >= delay
+    if (now - lastCall >= delay) {
+      fn.apply(context, args);
+      lastCall = now;
+    }
+  }
+}
