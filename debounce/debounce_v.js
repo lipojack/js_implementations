390cd9f88fad1b36128fec6fceb026f05c7751c7
lipojack <cp000123@gmail.com>
2025-07-08 15:20:27 +0800

    Debounce: Added ref check

--- debounce/debounce_v.js
+++ debounce/debounce_v.js
@@ -1,22 +1,30 @@
-import { onUnmounted} from "vue";
+import { onUnmounted, watch, isRef } from "vue";
 
 export const useDebounce = (fn, delay) => {
   let timer = null;
+  
+  // check if fn, delay is reactive
+  const getFn = () => isRef(fn) ? fn.value : fn;
+  const getDelay = () => isRef(delay) ? delay.value : delay;
 
   const debounced = (...args) => {
     if (timer) clearTimeout(timer);
     timer = setTimeout(() => {
-      fn(...args);
-    }, delay);
+      getFn()(...args);
+    }, getDelay());
   };
 
   const cancel = () => {
-    clearInterval(timer);
-  };
-
-  onUnmounted(() => {
     clearTimeout(timer);
-  });
+    timer = null;
+  };
   
-  return { debounced, cancel};
+  // watch [fn, delay] if they are reactive
+  if (isRef(fn) || isRef(delay)) {
+    watch([fn, delay], cancel);
+  }
+
+  onUnmounted(cancel);
+  
+  return { debounced, cancel };
 };
